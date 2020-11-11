352bbb6cd2b388a570aa8c93b2fc956fe38ec962
ninab95 <ninaborkovic.95@gmail.com>
2020-11-11 14:47:51 +0100

    link fixed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from './Layout.module.css';
 import { Brightness6Rounded } from "@material-ui/icons";
-import Link from 'next/Link';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
 const Layout = ({children, title = "World Ranks"}) => {
